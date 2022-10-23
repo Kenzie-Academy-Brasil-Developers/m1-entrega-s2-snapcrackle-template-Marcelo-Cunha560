9ae0b66b32d6e39e27e11b0be5d9ed36cfb91bd9
Marcelo-Cunha560 <113524572+Marcelo-Cunha560@users.noreply.github.com>
2022-10-23 12:32:51 -0400

    entrega realizada

---
+++ crakler.js
@@ -0,0 +1,23 @@
+function snapCrackle(maxValue){
+    let newString = ""
+   
+    for(i = 1; i <= maxValue; i++){
+        // i == 5
+        if(i % 2 != 0 && i % 5 == 0){
+            newString += "SnapCrackle"
+        }else if(i % 5 == 0) {
+            newString += "Crackle"
+        }else if(i % 2 != 0) {
+            newString += "Snap"
+        }else{
+            newString += i.toString()
+        }
+        if(i < maxValue){
+            newString += ", "
+        }
+    }
+    return newString
+
+}
+
+console.log(snapCrackle(1))
